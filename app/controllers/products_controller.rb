13e0fd34e0148ecf17db9ef10d934b7f28be8f5c
Boris Paillard <boris.paillard@gmail.com>
2016-03-03 10:23:49 +0100

    add resources routing and CRUD actions for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,18 @@
 class ProductsController < ApplicationController
-
   def index
     @products = Product.all
   end
-
   def show
     @product = Product.find(params[:id])
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-
   get "/team" => "pages#team"
   get "/contact" => "pages#join_us"
-  get "/products" => "products#index"
-  get "/products/:id" => "products#show"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products
+
+
+  # For details on the D
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
